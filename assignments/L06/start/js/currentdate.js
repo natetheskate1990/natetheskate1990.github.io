ba1951420e09195434ff847d5468fce4b1093f57
natetheskate1990 <35317992+natetheskate1990@users.noreply.github.com>
2018-02-22 12:39:23 -0600

    lots of change

---
+++ assignments/L06/start/js/currentdate.js
@@ -0,0 +1,13 @@
+var d = new Date();
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+var day = days[ d.getDay() ];
+var month = months[ d.getMonth() ];
+
+var year = d.getFullYear();
+var dayN = d.getDay();
+
+
+document.getElementById("currentdate").innerHTML = day + ", " + dayN + " " + month + " " + year;
