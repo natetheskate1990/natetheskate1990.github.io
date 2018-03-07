f29e2bdb4c20ba1e5c5d79f74a4c4fc94fab88a7
natetheskate1990 <35317992+natetheskate1990@users.noreply.github.com>
2018-03-06 22:55:15 -0600

    ah

---
+++ assignments/L08/js/hamburger.js
@@ -0,0 +1,9 @@
+function toggleNavMenu() {
+    var x = document.getElementById("navBox");
+    if (x.className === "show") {
+        x.className = "hide";
+    } else {
+            x.className = "show";
+            
+        }
+    }
---
+++ assignments/L08/js/windChill.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("high").innerHTML;
+var low = document.getElementById("low").innerHTML;
+x = parseInt(x);
+low = parseInt(low);
+var t = (x + low) / 2;
+
+var s = document.getElementById("speed").innerHTML;
+var s = parseInt(s);
+
+
+s = Math.pow(s, 0.16);
+var f = 35.74 + 0.6215 * t - 35.75 * s + 0.4275 * t * s;
+
+var wc = document.getElementById("windChill").innerHTML = Math.round(f);
