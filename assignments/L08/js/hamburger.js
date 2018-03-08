5496d5011aa7bba0bc8bf1d9da0711c8aad163cf
natetheskate1990 <35317992+natetheskate1990@users.noreply.github.com>
2018-03-08 12:31:39 -0600

    uh

--- assignments/L08/js/hamburger.js
+++ assignments/L08/js/hamburger.js
@@ -1,4 +1,4 @@
-function toggleNavMenu() {
+/*function toggleNavMenu() {
     var x = document.getElementById("navBox");
     if (x.className === "show") {
         x.className = "hide";
@@ -6,4 +6,7 @@ function toggleNavMenu() {
             x.className = "show";
             
         }
-    }
+    }*/
+function toggleNavMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
