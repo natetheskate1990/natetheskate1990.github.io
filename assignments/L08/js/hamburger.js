c5bffe3e821f02674a9884c51522f0bdee057a1a
natetheskate1990 <35317992+natetheskate1990@users.noreply.github.com>
2018-03-09 12:12:12 -0600

    push stuff

--- assignments/L08/js/hamburger.js
+++ assignments/L08/js/hamburger.js
@@ -1,12 +1,13 @@
-/*function toggleNavMenu() {
+function toggleNavMenu() {
     var x = document.getElementById("navBox");
     if (x.className === "show") {
         x.className = "hide";
-    } else {
+    } 
+    else {
             x.className = "show";
             
         }
-    }*/
-function toggleNavMenu() {
+    }
+/*function toggleNavMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
+}*/
