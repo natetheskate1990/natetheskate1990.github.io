1f7bb6ed382c1cba6b28a5b89383f313af5626ca
natetheskate1990 <35317992+natetheskate1990@users.noreply.github.com>
2018-03-12 20:20:37 -0500

    catching up
    53 files changed

---
+++ assignments/L09/js/hamburger.js
@@ -0,0 +1,13 @@
+function toggleNavMenu() {
+    var x = document.getElementById("navBox");
+    if (x.className === "show") {
+        x.className = "hide";
+    } 
+    else {
+            x.className = "show";
+            
+        }
+    }
+/*function toggleNavMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}*/
